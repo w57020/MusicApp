55db268e9aaa81bd1c8e2ca332075e7a44927aef
w57020 <zaq1@WSX>
2018-04-19 17:39:23 +0200

    wstecz

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.stud.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.stud.musicapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bTopSongs = findViewById(R.id.bTopSongs);
+        View.OnClickListener view;
+        bTopSongs.setOnClickListener(view);
+                public void onClick (View view){
+            Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+            startActivity(intent);
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -0,0 +1,26 @@
+package com.example.stud.musicapp.topsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stud.musicapp.R;
+
+public class TopSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_songs);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        onBackPressed();
+
+        return true;
+    }
+}
